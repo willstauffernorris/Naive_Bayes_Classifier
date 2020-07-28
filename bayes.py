1959b4bd18260c1293074db630191877f6407a88
willsn <willsn@gmail.com>
2020-07-27 18:10:57 -0600

    making progress

--- bayes.py
+++ bayes.py
@@ -7,6 +7,8 @@ You'll then use your implementation on an appropriate set of data and compare th
 '''
 
 
+
+
 ## Two important things to figure out:
 
 # 1) what format is the data I pass in going to be
@@ -38,13 +40,9 @@ class Gaussian_Naive_Bayes():
         self.y = y
 
 
-    ## check that there's something in the data (it's not none)
-
-
-
 
     # 1) Calculate prior probablilites **for each** class label
-        
+
        # 2) Calculate conditional probability with each attribute
         
         #probability that each feature is the way it is
@@ -107,3 +105,123 @@ class Gaussian_Naive_Bayes():
     # ]
 
     # X_test = ['male', 'male', 'female', 'female']
+
+
+from csv import reader
+
+#### import iris
+dataset = []
+with open('iris.csv', 'r') as file:
+    csv_reader = reader(file)
+    for row in csv_reader:
+        #if not row:
+           # continue
+        dataset.append(row)
+
+## This is the head of my dataset
+#print(dataset[0:5])
+
+
+## NEED TO MAKE SURE THE COLUMN NAMES ARE NOT IN IT
+#removing column names
+dataset = dataset[1:]
+
+
+#print("-------------")
+## Convert the strings that got read in to floats
+for i in range(len(dataset[0])-1):
+    for row in dataset:
+        row[i] = float(row[i].strip())
+
+#print(dataset[0:5])
+
+## Converting the string names of the target to int
+# for row in dataset:
+#     row[
+def string_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = {}
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    for row in dataset:
+        row[column] = lookup[row[column]]
+    return lookup
+
+
+string_to_int(dataset, len(dataset[0])-1)
+#print("-------------")
+#print(dataset[:5])
+
+
+## Train test split
+# from sklearn.model_selection import train_test_split
+
+
+
+## Separate by class
+def separate_by_class(data):
+    # creating a dictionary; each key is one of the possible classes
+    separated = {}
+    for i in range(len(data)):
+        row = dataset[i]
+        class_value = row[-1]
+        if class_value not in separated:
+            separated[class_value] = []
+        separated[class_value].append(row)
+    return separated
+
+
+separated = separate_by_class(dataset)
+# print("-------------")
+# for label in separated:
+#     print(label)
+#     for row in separated[label]:
+#         print(row)
+
+
+
+## Summarize dataset
+import numpy as np
+
+## get mean
+
+## calculate std deviation
+## calculate mean, std dev, and count for each column in dataset
+
+### SUMMARIZE
+def summarize_dataset(dataset):
+    summaries = [(np.mean(column), np.std(column), len(column)) for column in zip(*dataset)]
+    return summaries
+
+#print(summarize_dataset(dataset))
+
+#print(len(dataset))
+
+
+
+## summarize data by class
+summaries = {}
+for class_value, rows in separated.items():
+    summaries[class_value] = summarize_dataset(rows)
+
+for label in summaries:
+    print(label)
+    for row in summaries[label]:
+        print(row)
+
+
+
+
+
+## gaussian probability density function
+
+
+
+## class probabilities
+
+
+
+
+## put it all together in a more readable way
+## put it all into a class!!
